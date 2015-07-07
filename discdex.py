9c2492fce2b838129f24c0d2c4e949445f954cbc
weleoka <kai.weeks@gmail.com>
2015-07-07 21:54:25 +0200

    Reading filestats and writes to file.'

--- discdex.py
+++ discdex.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-import glob, os
-filetypes = "mp3 avi mpeg"
-path = "/home/bizles/python" 
-#input('Path to disc (ex. /media/bizles/SMALLBITS): ')
-title = input('Name of disc (ex. disc_01): ')
-outputFile = "test.txt"
-ticker = 0
+import os, fnmatch
+
+
+
+"""
+Config and options
+"""
+filetypes = "mp3 avi mpeg txt"
+outputFile = "discdex.txt"
+
 
 
 """
@@ -21,25 +24,80 @@ return:
     void
 """
 def discdexIt(batch, destination):
+
     with open(destination, "a") as myfile:
         myfile.write(batch)
         myfile.close()
 
 
-print ("Fisting files and folders under: " + path + " and writing to indexing file...")
 
-for filetype in filetypes.split(" "):
-    globlisting = path + '/*/*.' + filetype
-    results = glob.glob(globlisting)
-    ticker = ticker + len(results)
+"""
+Walk the directory tree and log add files found to list.
+
+parameters:
+    path: string. Root path to work from.
+    filetypes: string. The file types to list.
     
+return:
+    list
+"""
+def walkIt(path, filetype):
+
+    globlisting = path + '/*/*.' + filetype
+    results = []
+    
+    for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, '*.' + filetype):
+            results.append(os.path.join(root, filename))
+
     print ("Found " + str(len(results)) + " files with ending: " + filetype)
 
-    for name in results:
-        #print ('\t', name)
-        print ('\t' + os.path.basename(name))
-        entry = "\n" + os.path.basename(name) + ' ' + title
+    return results
+
+
+
+"""
+Create string of file stats append to list.
+
+parameters:
+    results: list. All the files found.
+    
+return:
+    entries: list. All the files with their stats as string.
+"""
+def entryIt(results):
+    entries = []
+
+    for result in results:
+        #print ('\t', result)
+        print ('\t' + os.path.basename(result))
+        entries.append('\n' + os.path.basename(result) + '\t' + sourceName  + '\t' + str(os.path.getsize(result)))
+
+    return entries
+
+
+
+
+"""
+MAIN
+"""
+ticker = 0
+
+path = "/home/bizles/python/discdex/" 
+#path = input('Path to disc (ex. /media/bizles/SMALLBITS): ')
+sourceName = input('Name of disc (ex. disc_01): ')
+
+print ("Listing files and folders under: " + path + " and writing to indexing file...")
+
+for filetype in filetypes.split(" "):
+    results = walkIt(path, filetype)
+    entries = entryIt(results)
+
+    ticker = ticker + len(entries)
+
+    for entry in entries:
         discdexIt(entry, outputFile)
+
 
 print ("Total " + str(ticker) + " files written to " + outputFile)
 
